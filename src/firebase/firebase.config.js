3daa46ad6202588c001ec5ef97f204d5ab060822
himel2535 <monwarhossanhimel@gmail.com>
2025-10-20 17:21:27 +0600

    All over all functionality with .env.local is olso done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,17 @@
-// Import the functions you need from the SDKs you need
+// firebase.init.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmymY87nOL40rBkoqx8S1o9wsYTSSUJJY",
-  authDomain: "dragon-news-firebase-a94bb.firebaseapp.com",
-  projectId: "dragon-news-firebase-a94bb",
-  storageBucket: "dragon-news-firebase-a94bb.firebasestorage.app",
-  messagingSenderId: "10511733141",
-  appId: "1:10511733141:web:fa499b4e2c0b5000b4269c",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
+console.log(import.meta.env.VITE_apiKey);
+
+
 const app = initializeApp(firebaseConfig);
 export default app;
